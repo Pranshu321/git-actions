13639b10cf92657bd831c6ad855ad64a61445b6a
Pranshu321 <pranshujain0111@gmail.com>
2025-09-13 16:48:50 +0530

    Implement basic calculator using readline for user input

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter first number: ", (first) => {
+  rl.question("Enter second number: ", (second) => {
+    const sum = parseFloat(first) + parseFloat(second);
+    console.log("Sum is " + sum);
+    rl.close();
+  });
+});
